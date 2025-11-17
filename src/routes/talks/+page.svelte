556c36711607c991da6ed35325cb161841115318
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-11-17 10:46:25 +0100

    dev: new: talks and teaching

--- src/routes/talks/+page.svelte
+++ src/routes/talks/+page.svelte
@@ -3,24 +3,46 @@
 <ul>
 	<!-- <li>2026-04: PyCon DE, Darmstadt, Germany</li> -->
 	<!-- <li>2026-05: EuroPython, Darmstadt, Germany</li> -->
-	<li>2024-09: GCB Bielefeld, Germany: Automatic kinetic model creation using mxlpy</li>
-	<li>2024-07: ECMTB Toledo, Spain: Secondary carbon-fixation improves photorespiration</li>
-	<li>2024-06: EPS2 Padova, Italy: Secondary carbon-fixation improves photorespiration</li>
-	<li>2022: ISGSB Innsbruck, Austria: Optimality principles of leaf venation patterns</li>
-	<li>2021: Crops in silico</li>
 	<li>
-		2019: MBP Dabringhausen, Germany: Metabolic Productivity of Photosynthetic glandular trichomes
+		2024-09: GCB (German Conference on Bioinformatics) Bielefeld, Germany: Automatic kinetic model
+		creation using mxlpy
 	</li>
-	<li>2018-09: ISGSB Tromsø, Norway: Optimality principles of leaf venation patterns</li>
+	<li>
+		2024-07: ECMTB (European Conference on Mathematical and Theoretical Biology) Toledo, Spain:
+		Secondary carbon-fixation improves photorespiration
+	</li>
+	<li>
+		2024-06: EPS2 (European Congress for Photosynthesis Research) Padova, Italy: Secondary
+		carbon-fixation improves photorespiration
+	</li>
+	<li>
+		2022-09: ISGSB (International Study Group for Systems Biology) Innsbruck, Austria: Optimality
+		principles of leaf venation patterns
+	</li>
+	<li>
+		2021-09: GCB (German Conference on Bioinformatics) Halle, Germany: How to build and analyse
+		mathematical models of biological systems using Python & modelbase (workshop)
+	</li>
+	<li>2021-06: Crops in silico, virtual: Optimality Principles in leaf venation patterns</li>
+	<li>
+		2019-02: MBP (Molecular Biology of Plants) Dabringhausen, Germany: Metabolic Productivity of
+		Photosynthetic glandular trichomes
+	</li>
+	<li>
+		2018-09: ISGSB (International Study Group for Systems Biology) Tromsø, Norway: Optimality
+		principles of leaf venation patterns
+	</li>
 </ul>
 
 <h2>Teaching</h2>
 
 <ul>
+	<li>2026-03: JII Open Hackathon Nigeria</li>
 	<li>2025-ws: Machine learning in natural sciences (16.02) at RWTH Aachen</li>
 	<li>2025-ws: Societal challenges datathon (42.17) at RWTH Aachen</li>
 	<li>2025-ss: Interdisciplinary Data Science (16.17) at RWTH Aachen</li>
 	<li>2024-ws: Machine learning in natural sciences (16.02) at RWTH Aachen</li>
+	<li>2023-ws: QBio202 - Deterministic processes in Biology at HHU Düsseldorf</li>
 	<li>2023-08: Embu summer school, Kenya</li>
 	<li>2022-09: Watamu summer school, Kenya</li>
 	<li>2019-ss: M4455 - Mathematical modelling at HHU Düsseldorf</li>
